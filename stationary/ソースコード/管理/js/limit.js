9ab49e99df81a8ff5340c7f515fdece64f99e5da
Aso2001154 <83046798+Aso2001154@users.noreply.github.com>
2021-12-25 11:03:02 +0900

    Add files via upload

---
+++ stationary/ソースコード/管理/js/limit.js
@@ -0,0 +1,61 @@
+const app = new Vue({
+    el:'#app',
+    data(){
+        return{
+            id:'',
+            pass:'',
+            credit:''
+        };
+    },
+    computed:{
+        isInValidId(){
+            if(this.id.match(/[a-zA-Z0-9]{8}/)){
+                if(this.id.match(/[a-zA-Z0-9]{9}/)){
+                    return true;
+                }else{
+                    if(this.id.match(/[あ-んァ-ヾA-Z-ー*@/()$#&!%<>亜-熙０-９]/)){
+                        return true;
+                    }else {
+                        if(this.id.match(/[a-zA-Z]/)&&this.id.match(/[0-9]/)) {
+                            return false;
+                        }else{
+                            return true;
+                        }
+                    }
+                }
+            }else{
+                return true;
+            }
+        },
+        isInValidPass(){
+            if(this.pass.match(/[0-9]{4}/)){
+                if(this.pass.match(/[0-9]{5}/)){
+                    return true;
+                }else{
+                    if(this.pass.match(/[あ-んァ-ヾa-zA-ZA-Z-ー*@/()$#&!%<>亜-熙０-９]/)){
+                        return true;
+                    }else {
+                        return false;
+                    }
+                }
+            }else{
+                return true;
+            }
+        },
+        isInValidCredit(){
+            if(this.credit.match(/[0-9]{16}/)){
+                if(this.credit.match(/[0-9]{17}/)){
+                    return true;
+                }else{
+                    if(this.credit.match(/[-]/)){
+                        return true;
+                    }else {
+                        return false;
+                    }
+                }
+            }else{
+                return true;
+            }
+        }
+    }
+});
